405742c4685bcac3e30826678e8ca02759286959
Dominic Sagers <d.sagers@student.maastrichtuniversity.nl>
2023-03-02 10:56:38 +0100

    Added some logic for days and times

--- src/main/java/DigitalAssistant/skillLogic/Parser.java
+++ src/main/java/DigitalAssistant/skillLogic/Parser.java
@@ -6,27 +6,45 @@ public class Parser {
     public String lastParsedString;
     public Time.Month month;
     public Time time = new Time();
+    public int day;
+    public int year = 2020;
+    public boolean isLeap;
 
-    public Parser(){}
+    public Parser(){
+        isLeap = Time.isLeapYear(year);
+    }
 
     public void getSkillInputs(String stringToParse){
         Scanner scanner = new Scanner(stringToParse);
+        String previousString = "";
         scanner.useDelimiter(" ");
         while(scanner.hasNext()){
             String currentSring = scanner.next();
-            //System.out.println(currentSring);
+
+            /*If a month is detected, save it and check to see if a day is entered before or after and save it, 
+                otherwise display error
+             */
             time.isMonth(currentSring, this);
             
+    
+            previousString = currentSring;
         }
         scanner.close();
     }
 
+    public static int getInt(String str){
+        String strReg = str.replaceAll("[^0-9:]", "");
+        if(strReg.equals("") || strReg.contains(":")){return -1;}
+        return Integer.valueOf(strReg);
+    }
     
     public static void main(String[] args) {
         Parser parser = new Parser();
         String example = "Place a meeting on the 14th of February";
         parser.getSkillInputs(example);
         System.out.println(parser.month);
+        System.out.println(parser.day);
+      
     }
 
 }
--- src/main/java/DigitalAssistant/skillLogic/Time.java
+++ src/main/java/DigitalAssistant/skillLogic/Time.java
@@ -3,8 +3,7 @@ package DigitalAssistant.skillLogic;
 public class Time {
     public static Month[] months = {Month.JANUARY, Month.FEBRUARY, Month.APRIL, Month.MAY, Month.JUNE, Month.JULY, Month.AUGUST, Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER, Month.DECEMBER};
 
-
-    public enum Month{
+    public static enum Month{
         JUNE("june", 6),JULY("july", 7),AUGUST("august", 8),
          SEPTEMBER("september", 9), OCTOBER("october", 10)
         ,NOVEMBER("november", 11), DECEMBER("December", 12),
@@ -34,6 +33,47 @@ public class Time {
             }
         }
         return false;
+    }
+
+    public static boolean dayIsValid(int day, Month month, boolean isLeap){
+        if(day < 1 || day > 31){
+            return false;
+        }
+        switch(month.getValue()){
+            case 2:
+                if(isLeap){
+                    if(day <= 29){return true;}
+                }else{
+                    if(day <= 28){return true;}
+                }
+                return false;
+            case 4: 
+                if(day<=30){return true;}
+                return false;
+            case 6:
+                if(day<=30){return true;}
+                return false;
+            case 9:
+                if(day<=30){return true;}
+                return false;
+            case 11: 
+                if(day<=30){return true;}
+                return false;
+            }
+
+        return true;
+    }
+    
+    //https://nrc.canada.ca/en/certifications-evaluations-standards/canadas-official-time/what-years-are-leap-years
+    public static boolean isLeapYear(int year){
+        if(year % 400 == 0){
+            return true;
+       }else if(year % 100 == 0){
+            return false; 
+       }else if(year % 4 == 0){
+            return true;
+       }
+       return false;
     }
 
     /*TODO:
