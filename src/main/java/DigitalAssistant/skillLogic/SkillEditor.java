dce186ff3dcabb8c2b461b30edb7d267ed75adbc
alex07 <alexfrerichs@t-online.de>
2023-03-08 12:29:24 +0100

    SkillEditor: Placeholder extraction done but not tested yet

--- src/main/java/DigitalAssistant/skillLogic/SkillEditor.java
+++ src/main/java/DigitalAssistant/skillLogic/SkillEditor.java
@@ -55,32 +55,45 @@ public class SkillEditor {
         try (BufferedReader reader = new BufferedReader(new FileReader("skills.txt"))) {
 
             String currentLine;
+            String phKey = "";       // placeholder i.e. !DAY?
+            ArrayList<String> phValues = new ArrayList<>();     // list of placeholders
+    
             currentLine = reader.readLine();
-            while (!currentLine.equals(".....")) {//Until the end of defined skill
-                if(currentLine.equals("*****")){// '*****' indicates the start of a new skill
+            while (!currentLine.equals(".....")) {      //Until the end of defined skill
+                if(currentLine.equals("*****")){        // '*****' indicates the start of a new skill
                     String skillName = reader.readLine();
                     String skillPrototype = reader.readLine();
-                    ArrayList<String> placeholders = new ArrayList<>();// list of placeholders
+                    
                     String ph = reader.readLine();
                     while(!ph.equals("*****") || !ph.equals(".....")){
                         
-                        //
-                        // TODO: extract the placeholders
-                        //
+                        // TODO: test placeholder extraction
+
                         Scanner scanner = new Scanner(ph);
                         scanner.useDelimiter(" ");
+                        phKey = scanner.next(); 
+                        System.out.println(phKey);
+                        String temp = scanner.next();
+                        Scanner scan2 = new Scanner(temp);
+                        scan2.useDelimiter("||");
 
+                        while(scan2.hasNext()){
+                            phValues.add(scanner.next());       // add placeholder values to arrayList
+                        }
 
-                        ph = reader.readLine();
+                        ph = reader.readLine();        // what is this for? reading next line in file?
                         scanner.close();
+                        scan2.close();
                     }
+                    
                     Skill newSkill = new Skill(skillName, skillPrototype);
                     //set Placeholders
+                    newSkill.setPlaceholderValue(phKey, phValues);
                     skills.add(newSkill);
                     currentLine = ph;
                 }
 
-                
+
             }
         } catch (IOException e) {
             System.err.println("Error reading skills from file: " + e.getMessage());
@@ -88,35 +101,54 @@ public class SkillEditor {
     }
 
     public static void main(String[] args) {
-    //     SkillEditor skillEditor = new SkillEditor("anan.txt");
+    //  SkillEditor skillEditor = new SkillEditor("anan.txt");
         
-    //     // Create a new skill with placeholders for day and time
-    //     Skill skill = new Skill("Lecture","Which lectures are there on ¡DAY¿ at ¡TIME¿");
-    //     skill.setPlaceholderValue("¡DAY¿", new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"});
-    //     skill.setPlaceholderValue("¡TIME¿", new String[]{"9", "11", "15"});
+        // Create a new skill with placeholders for day and time
+        //Skill skill = new Skill("Lecture","Which lectures are there on ¡DAY¿ at ¡TIME¿");
+        Skill skill = new Skill("Lecture","Schedule a meeting on ¡DAY¿ at ¡TIME¿");
+        //skill.setPlaceholderValue("¡DAY¿", new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"});
+        //skill.setPlaceholderValue("¡TIME¿", new String[]{"9", "11", "15"});
         
-    //     // Define the action for the skill
-    //     skill.setAction(new SkillAction() {
-    //         @Override
-    //         public void performAction(HashMap<String, String[]> arguments) {
-    //             String[] day = arguments.get("$DAY$");
-    //             String[] time = arguments.get("¡TIME¿");
+        // Define the action for the skill
+        SkillAction action = new SkillAction() {
+
+            @Override
+            public void performAction(HashMap<String, String> parameters) {
+                // TODO Auto-generated method stub
+                // store meetings in some data structure
+
+                System.out.println("Schedule a new event for tomorrow 9 AM.");
+                //parameters.put(day, time);
+                System.out.println("Success.");
+            }
+            
+        };
+
+        skill.setAction(action);
+        
+        // skill.setAction( new SkillAction() {
+
+        //     @Override
+        //     public void performAction(HashMap<String, String[]> arguments) {
+        //         String[] day = arguments.get("$DAY$");
+        //         String[] time = arguments.get("¡TIME¿");
                 
-    //             // Perform the action based on the day and time
-    //             if (day.equals("Monday") && time.equals("9")) {
-    //                 System.out.println("There is a Math lecture at 9 AM on Monday.");
-    //             } else if (day.equals("Tuesday") && time.equals("11")) {
-    //                 System.out.println("There is a Physics lecture at 11 AM on Tuesday.");
-    //             } else if (day.equals("Wednesday") && time.equals("15")) {
-    //                 System.out.println("There is a Chemistry lecture at 3 PM on Wednesday.");
-    //             } else {
-    //                 System.out.println("There are no lectures scheduled for that day and time.");
-    //             }
-    //         }
-    //     });
+        //         // Perform the action based on the day and time
+        //         if (day.equals("Monday") && time.equals("9")) {
+        //             System.out.println("There is a Math lecture at 9 AM on Monday.");
+        //         } else if (day.equals("Tuesday") && time.equals("11")) {
+        //             System.out.println("There is a Physics lecture at 11 AM on Tuesday.");
+        //         } else if (day.equals("Wednesday") && time.equals("15")) {
+        //             System.out.println("There is a Chemistry lecture at 3 PM on Wednesday.");
+        //         } else {
+        //             System.out.println("There are no lectures scheduled for that day and time.");
+        //         }
+        //     }
+
+        // });
         
-    //     // Test the skill
-    //     String input = "Which lectures are there on Monday at 9?";
-    //     skill.performAction();
+        // Test the skill
+        String input = "Which lectures are there on Monday at 9?";
+        skill.performAction();
      }
 }
