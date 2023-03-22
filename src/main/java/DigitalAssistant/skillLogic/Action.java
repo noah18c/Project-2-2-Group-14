387292caab814f5d5e2f4caef28117578ef71da5
Dominic Sagers <d.sagers@student.maastrichtuniversity.nl>
2023-03-22 10:22:29 +0100

    Merge commit 'adfa5f6e4e0c29bb5413c6acdba44597ee66a894' into 1-BACKEND-01

---
+++ src/main/java/DigitalAssistant/skillLogic/Action.java
@@ -0,0 +1,72 @@
+package DigitalAssistant.skillLogic;
+
+import java.util.HashMap;
+
+public class Action {
+    private String actionType;
+    private String actionInput;
+    private HashMap<String,String> actionValues;
+
+    public Action(String actionType, String actionInput , HashMap<String,String> actionValues){
+        this.actionInput = actionInput;
+        this.actionType = actionType;
+        this.actionValues = actionValues;
+        //System.out.println(actionType + " " + actionInput + " " + actionValues);
+    }
+
+    public void triggerAction(){
+        loadAction();
+    }
+
+    public void loadAction(){
+        if (actionType.equals(null)) {
+            UnknownAction();
+        }
+        else if(actionType.equals("|Search|")){
+            GoogleSearchAction();
+        }
+        else if(actionType.equals("|Print|")){
+            PrintAction();
+        }
+    }
+
+    private void UnknownAction() {
+        System.out.println("I'm sorry, there is no action");
+    }
+
+    private void GoogleSearchAction() {
+        System.out.println("Searching Google for ...");
+    }
+
+    private void CalendarAction() {
+        System.out.println("Calender Action");
+    }
+
+    private void PrintAction(){
+        System.out.println(actionInput);
+    }
+
+    public String getActionType() {
+        return actionType;
+    }
+
+    public String getActionInput() {
+        return actionInput;
+    }
+
+    public HashMap<String, String> getActionValues() {
+        return actionValues;
+    }
+
+    public void setActionType(String actionType) {
+        this.actionType = actionType;
+    }
+
+    public void setActionInput(String actionInput) {
+        this.actionInput = actionInput;
+    }
+
+    public void setActionValues(HashMap<String, String> actionValues) {
+        this.actionValues = actionValues;
+    }
+}
