f8c5933d94b871cdfd09c8dd404e6e406c4b0929
Dominic Sagers <d.sagers@student.maastrichtuniversity.nl>
2023-05-13 12:49:32 +0200

    Made recursive version of CFG work with "@INPUT"

--- src/main/java/DigitalAssistant/skillLogic/CFG/CFGParser.java
+++ src/main/java/DigitalAssistant/skillLogic/CFG/CFGParser.java
@@ -1,6 +1,7 @@
 package DigitalAssistant.skillLogic.CFG;
 
 import java.util.*;
+
 import DigitalAssistant.skillLogic.SkillEditor;
 import DigitalAssistant.skillLogic.Word;
 import DigitalAssistant.skillLogic.Skill;
@@ -27,7 +28,7 @@ public class CFGParser {
         words = processStringCFG(sentence);
         
         for (int i = 0; i < grammar.get(0).expansions.size(); i++) {
-            //System.out.println(i);
+            System.out.println(i);
             ArrayList<String> clonedWords = new ArrayList<String>();
             clonedWords.addAll(words);
             
@@ -35,9 +36,7 @@ public class CFGParser {
                 skillName = grammar.get(0).expansions.get(i);
                 break;
             }
-            else{
-                placeholderValues = new ArrayList<>();
-            }
+            
         }
         
         System.out.println("---------");
@@ -47,106 +46,96 @@ public class CFGParser {
     }
 
     public boolean findRule(Rule rule, ArrayList<String> words){
-        //System.out.println(rule.nonterminal + "  " + words.toString());
+        System.out.println(rule.nonterminal + "  " + words.toString());
 
-        // if(words.size() == 0){//IF WE DELETED EVERYTHING WE SHOULD BE GOOD AND HAPPY AND RETURN
-        //     System.out.println("1");
+        if(words.size() == 0){//IF WE DELETED EVERYTHING WE SHOULD BE GOOD AND HAPPY AND RETURN
+            System.out.println("1");
 
-        //     return true;
+            return true;
 
-       // }else{
-            ArrayList<String> next = new ArrayList<String>();
-            next.addAll(words);
+        }else{
+            if(rule.expansions.contains(words.get(0))){//WE FOUND A NON-TERMINAL, SO WE DELETE AND CONTINUE
+                ArrayList<String> next = new ArrayList<String>();
+                next.addAll(words);
+                next.remove(words.get(0));
 
-            //Remove every expansion words of rule from sentence
-            for (int i = 0; i < rule.expansions.size(); i++) {
-                for (int j = 0; j < next.size(); j++) {
-                    if(rule.expansions.get(i).equalsIgnoreCase(next.get(j))){
-                        next.remove(j);
-                    }   
-                }
-            }
-            System.out.println(next.size() + " " + rule.placeholders.toString());
-
-            //if(next.size() == rule.placeholders.size()){
-
-                for (int i = 0; i < next.size(); i++) {
-                    if(checkPlaceholders(rule, next.get(i),false)){
-                        next.remove(i);
-                        i = i-1;
+                if(words.size() == 0){
+                    System.out.println("2");
+                        return true;
                     }
-                }
 
-                for (int i = 0; i < next.size(); i++) {
-                    if(checkPlaceholders(rule, next.get(i), true)){
-                        next.remove(i);
-                        i = i-1;
+
+                return findRule(rule, next);
+                
+            }else{//WE DIDNT FIND A TERMINAL
+            
+                boolean placeholderFound = false;
+                if(checkPlaceholders(rule, words.get(0))){
+                    placeholderFound = true;
+                    
+                    ArrayList<String> next = new ArrayList<String>();
+                    next.addAll(words);
+                    next.remove(words.get(0));
+
+                    if(words.size() == 0){
+                        System.out.println("3");
+
+                        return true;
                     }
+                    
+                    return findRule(rule, next);
+                }else if(isInputRule(rule)){
+
+                    System.out.println("isInputRule is true");
+                    placeholderFound = true;
+                    placeholderValues.add(words.get(0));
+
+                    ArrayList<String> next = new ArrayList<String>();
+                    next.addAll(words);
+                    next.remove(words.get(0));
+
+                    if(words.size() == 0){
+                        System.out.println("4");
+
+                        return true;
+                    }
+                    
+                    return findRule(rule, next);
+
                 }
-
-            //}
-
-            if(next.size() == 0){
-                return true;
-            }
-            else{
-                return false;
-            }
-            
-        //     if(rule.expansions.contains(words.get(0))){//WE FOUND A NON-TERMINAL, SO WE DELETE AND CONTINUE
-        //         ArrayList<String> next = new ArrayList<String>();
-        //         next.addAll(words);
-        //         next.remove(words.get(0));
-
-        //         if(words.size() == 0){
-        //             System.out.println("2");
-        //                 return true;
-        //             }
-
-
-        //         return findRule(rule, next);
                 
-        //     }else{//WE DIDNT FIND A TERMINAL
-            
-        //         boolean placeholderFound = false;
-        //         if(checkPlaceholders(rule, words.get(0))){
-        //             placeholderFound = true;
-                    
-        //             ArrayList<String> next = new ArrayList<String>();
-        //             next.addAll(words);
-        //             next.remove(words.get(0));
-
-        //             if(words.size() == 0){
-        //                 System.out.println("3");
-
-        //                 return true;
-        //             }
-                    
-        //             return findRule(rule, next);
-        //         }
-                
-        //         if(placeholderFound != true){
-        //             placeholderValues = new ArrayList<String>();//resets any previously set placeholders
-        //             return false;
-        //         }
-        //     }
-        //}
-        // return false;
+                if(placeholderFound != true){
+                    placeholderValues = new ArrayList<String>();//resets any previously set placeholders
+                    return false;
+                }
+            }
+        }
+        return false;
     }
 
-    public boolean checkPlaceholders(Rule rule, String wordToCheck , boolean input){
+    public boolean isInputRule(Rule rule){
+        for(Skill skill : skills){
+            if(rule.nonterminal.equals(skill.getName())){
 
-        
-        for(String placeholder : rule.placeholders){
-            Rule currentPlaceholder = getSkillRule(placeholder);
-            if(currentPlaceholder.expansions.size() == 1){
-                if(currentPlaceholder.expansions.get(0).equals("@INPUT")){
-                    if(input){
-                        placeholderValues.add(wordToCheck);
+                Set<String> keySet = skill.getPlaceholders().keySet();//Making the hashMap iterable
+                ArrayList<String> keyList = new ArrayList<>(keySet);
+
+                for(String key : keySet){
+                    if(skill.getPlaceholders().get(key).get(0).equals("@INPUT")){
                         return true;
                     }
                 }
+
             }
+        }
+
+        return false;
+    }
+
+    public boolean checkPlaceholders(Rule rule, String wordToCheck){
+        for(String placeholder : rule.placeholders){
+            Rule currentPlaceholder = getSkillRule(placeholder);
+
             for (int i = 0; i < currentPlaceholder.expansions.size(); i++) {
                 if(currentPlaceholder.expansions.get(i).equalsIgnoreCase(wordToCheck)){
                     placeholderValues.add(wordToCheck);
@@ -156,7 +145,6 @@ public class CFGParser {
         }
         return false;
     }
-
 
     //If rule cannot be found, null will be returned.
     public Rule getSkillRule(String ruleName){
@@ -177,7 +165,6 @@ public class CFGParser {
 
         for(Skill skill : skills){
             startRule.expansions.add(skill.getName());
-            
             Rule newRule = new Rule(skill.getName(), processStringCFG(skill.getPrototype()));
             
             rules.add(newRule);
@@ -231,6 +218,6 @@ public class CFGParser {
         CFGParser parser = new CFGParser(skillEditor.getSkills());
         ArrayList<Rule> grammar = parser.grammar;
 
-        parser.parse("What is the distance between ankara to maas?");     
+        parser.parse("What is the distance between Maastricht to Dubai?");     
     }
 }
